38ed8fc9aecc24df4208272e3e2a44f1df7d36ce
Esteban Madrazo <31863684+EstebanMadrazo@users.noreply.github.com>
2018-01-16 12:05:48 -0600

    cambio

--- fibonacci.c
+++ fibonacci.c
@@ -15,7 +15,7 @@ int main(int argc, char const *argv[]) {
       sigNumero = primerNum + segundoNum;
       primerNum = segundoNum;
       primerNum = segundoNum;
-      segundoNum = sigNumero;
+      segundoNum = sigNumer;
     }
     printf("%d\n", sigNumero);
   }
